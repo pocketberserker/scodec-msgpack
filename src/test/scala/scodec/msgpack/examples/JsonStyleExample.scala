71be523cf5645e8d7c491c93c9c35d85e70f4995
pocketberserker <pocketberserker@gmail.com>
2014-10-18 13:53:16 +0900

    add example

---
+++ src/test/scala/scodec/msgpack/examples/JsonStyleExample.scala
@@ -0,0 +1,32 @@
+package scodec
+package msgpack
+package examples
+
+import scodec.codecs._
+import org.scalacheck.Prop._
+import org.scalacheck.Arbitrary._
+import org.scalatest.prop.Checkers
+import MessagePackArbitrary._
+
+class JsonStyleExample extends TestSuite {
+
+  sealed abstract class Setting
+  case class IsCompact(v: Boolean) extends Setting
+  case class SchemaSize(v: Int) extends Setting
+
+  implicit val stringS = Serialize.string
+  implicit val settingSerializer = new Serialize[Setting] {
+    def pack(v: Setting) = v match {
+      case IsCompact(v) => Serialize.bool.pack(v)
+      case SchemaSize(v) => Serialize.int.pack(v)
+    }
+    def unpack(v: MessagePack) =
+      Serialize.bool.unpack(v).map(IsCompact)
+        .orElse(Serialize.int.unpack(v).map(SchemaSize))
+  }
+
+  "json style data" should "encode and decode" in {
+    val input = Map("compact" -> IsCompact(true), "schema" -> SchemaSize(0))
+    roundtrip(msgpack.map[String, Setting], input)
+  }
+}
