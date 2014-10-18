172023c5014f5dc3cf0f5c5e262033edc2f9973d
pocketberserker <pocketberserker@gmail.com>
2014-10-18 13:59:55 +0900

    abstract

--- src/test/scala/scodec/msgpack/TestSuite.scala
+++ src/test/scala/scodec/msgpack/TestSuite.scala
@@ -5,7 +5,7 @@ import scodec.bits.BitVector
 import org.scalatest.FlatSpec
 import org.scalatest.DiagrammedAssertions
 
-class TestSuite extends FlatSpec with DiagrammedAssertions {
+abstract class TestSuite extends FlatSpec with DiagrammedAssertions {
 
   // https://github.com/scodec/scodec/blob/ae93a6e30a2a85796586c64ec7b84b1527c488ef/src/test/scala/scodec/CodecSuite.scala#L18
   def roundtrip[A](codec: Codec[A], a: A) = {
