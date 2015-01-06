3ccda60335e05c88ad8a74aef5e6ddcd24c4f1fa
pocketberserker <pocketberserker@gmail.com>
2015-01-06 21:10:03 +0900

    update setting

--- project/build.scala
+++ project/build.scala
@@ -1,39 +1,75 @@
 import sbt._
 import Keys._
-import spray.revolver.RevolverPlugin._
+import sbtrelease._
+import xerial.sbt.Sonatype._
+import ReleaseStateTransformations._
+import com.typesafe.sbt.pgp.PgpKeys
 
 object ScodecMsgPackBuild extends Build {
   import Dependencies._
 
-  lazy val buildSettings = Defaults.defaultSettings ++
-    Revolver.settings ++
-    Seq(
-      scalaVersion := "2.11.4",
-      crossScalaVersions := Seq("2.10.4", scalaVersion.value),
-      resolvers += Resolver.sonatypeRepo("releases"),
-      resolvers += "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases",
-      scalacOptions ++= (
-        "-deprecation" ::
-        "-unchecked" ::
-        "-Xlint" ::
-        "-language:existentials" ::
-        "-language:higherKinds" ::
-        "-language:implicitConversions" ::
+  lazy val buildSettings = Seq(
+    ReleasePlugin.releaseSettings,
+    sonatypeSettings
+  ).flatten ++ Seq(
+    scalaVersion := "2.11.4",
+    crossScalaVersions := Seq("2.10.4", scalaVersion.value),
+    resolvers += Opts.resolver.sonatypeReleases,
+    scalacOptions ++= (
+      "-deprecation" ::
+      "-unchecked" ::
+      "-Xlint" ::
+      "-language:existentials" ::
+      "-language:higherKinds" ::
+      "-language:implicitConversions" ::
+      Nil
+    ),
+    scalacOptions ++= {
+      if(scalaVersion.value.startsWith("2.11"))
+        Seq("-Ywarn-unused", "-Ywarn-unused-import")
+      else
         Nil
+    },
+    libraryDependencies ++= Seq(
+      scodec,
+      scalatest % "test",
+      msgpackJava % "test",
+      scalacheck % "test"
+    ),
+    ReleasePlugin.ReleaseKeys.releaseProcess := Seq[ReleaseStep](
+      checkSnapshotDependencies,
+      inquireVersions,
+      runClean,
+      runTest,
+      setReleaseVersion,
+      commitReleaseVersion,
+      tagRelease,
+      ReleaseStep(
+        action = state => Project.extract(state).runTask(PgpKeys.publishSigned, state)._1,
+        enableCrossBuild = true
       ),
-      scalacOptions ++= {
-        if(scalaVersion.value.startsWith("2.11"))
-          Seq("-Ywarn-unused", "-Ywarn-unused-import")
-        else
-          Nil
-      },
-      libraryDependencies ++= Seq(
-        scodec,
-        scalatest % "test",
-        msgpackJava % "test",
-        scalacheck % "test"
-      )
-    )
+      setNextVersion,
+      commitNextVersion,
+      pushChanges
+    ),
+    organization := "com.github.pocketberserker",
+    licenses := Seq("MIT License" -> url("http://www.opensource.org/licenses/mit-license.php")),
+    pomExtra :=
+      <developers>
+        <developer>
+          <id>pocketberserker</id>
+          <name>Yuki Nakayama</name>
+          <url>https://github.com/pocketberserker</url>
+        </developer>
+      </developers>
+      <scm>
+        <url>git@github.com:pocketberserker/scodec-msgpack.git</url>
+        <connection>scm:git:git@github.com:pocketberserker/scodec-msgpack.git</connection>
+        <tag>{ "v" + version.value }</tag>
+      </scm>
+    ,
+    description := "yet another msgpack implementation"
+  )
 
   lazy val msgpack = Project(
     id = "scodec-msgpack",
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,3 @@
-addSbtPlugin("io.spray" % "sbt-revolver" % "0.7.2")
+addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8.1")
+addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.8.5")
+addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "0.2.1")
