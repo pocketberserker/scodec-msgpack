68dedf960ad16411f2a3152660fb3ff167298a6e
pocketberserker <pocketberserker@gmail.com>
2016-03-04 14:08:32 +0900

    move class

--- shared/src/test/scala/scodec/msgpack/examples/CaseClassExample.scala
+++ shared/src/test/scala/scodec/msgpack/examples/CaseClassExample.scala
@@ -5,13 +5,12 @@ package examples
 import Serialize._
 import SerializeAuto._
 
+sealed abstract class Setting extends Product with Serializable
+final case class IsCompact(v: Boolean) extends Setting
+final case class SchemaSize(v: Int) extends Setting
+final case class MsgPackExample(compact: IsCompact, schema: SchemaSize)
+
 class CaseClassExample extends TestSuite {
-
-  sealed abstract class Setting extends Product with Serializable
-  case class IsCompact(v: Boolean) extends Setting
-  case class SchemaSize(v: Int) extends Setting
-
-  final case class MsgPackExample(compact: IsCompact, schema: SchemaSize)
 
   "case class" should "encode and decode" in {
     val input = MsgPackExample(IsCompact(true), SchemaSize(0))
